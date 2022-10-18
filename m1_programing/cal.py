e7ac2eff09298c4137a2b34121e9bbecb627e75d
Toddbear20 <sch01483@gmail.com>
2022-10-18 14:52:42 +0900

    머신러닝 및 머신러닝 응용

--- m1_programing/cal.py
+++
@@ -1,5 +0,0 @@
-
-def calcu(x,y):
-    return x + y, x*y, x-y, x/y
-if __name__ == "__main__":
-    print(calcu(3,4))
--- m1_programing/cal4.py
+++
@@ -1,4 +0,0 @@
-
-def cal(x,y):
-    return x + y, x * y, x - y, round(x / y,2)
-print(cal(5,3))
